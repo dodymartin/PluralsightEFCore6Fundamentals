961985021e3143340b7e8eaeb350d0703c2b269e
Cody Rush <cody_rush@cargill.com>
2023-11-20 17:17:26 -0600

    Finished with Creating Data Model

---
+++ M02 Building Your FIrst App/Before/PublisherData/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData;
+public class PubContext : DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+}
