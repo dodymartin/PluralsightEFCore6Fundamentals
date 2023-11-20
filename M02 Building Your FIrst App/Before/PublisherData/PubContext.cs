ced51fb7136d17b1f5c9e202ed6a64523e68d5e7
Cody Rush <cody_rush@cargill.com>
2023-11-20 17:24:15 -0600

    A little bit more.

--- M02 Building Your FIrst App/Before/PublisherData/PubContext.cs
+++ M02 Building Your FIrst App/Before/PublisherData/PubContext.cs
@@ -7,4 +7,11 @@ public class PubContext : DbContext
 {
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
+            );
+    }
 }
