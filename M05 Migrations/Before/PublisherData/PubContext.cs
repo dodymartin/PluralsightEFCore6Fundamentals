4db9bb0dae0b9028cdb907cce9d1693f53d306d4
dodymartin <cody.rush@outlook.com>
2023-12-06 21:57:35 -0600

    Module 5 completed.

--- M05 Migrations/Before/PublisherData/PubContext.cs
+++ M05 Migrations/Before/PublisherData/PubContext.cs
@@ -1,19 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PublisherDomain;
 
-namespace PublisherData
+namespace PublisherData;
+
+public class PubContext : DbContext
 {
-    public class PubContext : DbContext
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Author> Authors { get; set; }
-        public DbSet<Book> Books { get; set; }
+        optionsBuilder.UseSqlServer(
+          "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
+        );
+    }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>().HasData(
+            new Author { Id = 1, FirstName = "Rhoda", LastName = "Lerman" });
+
+        var authorList = new Author[]
         {
-            optionsBuilder.UseSqlServer(
-              "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
-            );
-        }
+            new Author { Id = 2, FirstName = "Ruth", LastName = "Ozeki" },
+            new Author { Id = 3, FirstName = "Sofia", LastName = "Segovia" },
+            new Author { Id = 4, FirstName = "Ursula K.", LastName = "LeGuin" },
+            new Author { Id = 5, FirstName = "Hugh", LastName = "Howey" },
+            new Author { Id = 6, FirstName = "Isabelle", LastName = "Allende" }
+        };
+        modelBuilder.Entity<Author>().HasData(authorList);
 
+        //modelBuilder.Entity<Book>().HasData(
+        //    new Book { BookId = 1, Title = "Learn Azure in a Month of Lunches", AuthorId = 1 },
+        //    new Book { BookId = 2, Title = "Hands-On Azure for Developers", AuthorId = 1 },
+        //    new Book { BookId = 3, Title = "Learn Azure in a Month of Lunches", AuthorId = 2 },
+        //    new Book { BookId = 4, Title = "Hands-On Azure for Developers", AuthorId = 2 },
+        //    new Book { BookId = 5, Title = "Learn Azure in a Month of Lunches", AuthorId = 3 },
+        //    new Book { BookId = 6, Title = "Hands-On Azure for Developers", AuthorId = 3 });
     }
 }
